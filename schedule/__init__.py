9b0939f063313cdccaec70aa51babe0bf3821e61
evdokimov <o.evdokimov@corp.mail.ru>
2019-06-10 01:56:10 +0300

    fix

--- schedule/__init__.py
+++ schedule/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile('config.pyxx')
+    app.config.from_pyfile('config.py')
     @app.route("/")
     def schedule():
         return 'schedule'

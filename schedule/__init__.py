ab9ae2ef79395ee250a058c973d34d3736fab534
o-evdokimov <elegek@gmail.com>
2019-06-15 02:29:58 +0300

    ok

--- schedule/__init__.py
+++ schedule/__init__.py
@@ -2,12 +2,15 @@
 # init Flask server
 
 from flask import Flask
+#from config import Configuration
 #from schedule.model import db
 
 
 def create_app():
+    #from config import Configuration
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
+    #app.config.from_object(Configuration)
     #db.init_app(app)
     @app.route("/")
     @app.route("/index/")
--- schedule/app.py
+++ schedule/app.py
@@ -2,15 +2,12 @@
 # init Flask server
 
 from flask import Flask
+from config import Configuration
 
 
 app = Flask(__name__)
 #app.config.from_pyfile('config.py')
+app.config.from_object(Configuration)
 
-@app.route("/")
-def schedule():
-   return 'schedule'
-   #return app
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run()

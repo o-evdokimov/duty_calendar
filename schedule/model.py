5c59a15fe593f64d9c05c118177c0284f07c572c
o-evdokimov <elegek@gmail.com>
2019-06-16 23:13:08 +0300

    ok

--- schedule/model.py
+++ schedule/model.py
@@ -2,29 +2,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy
 
-class Duty_Event(db.Model):
+class DutyEvent(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     event_type = db.Column(db.String, foreign_key=True, nullable=False)
     duty_person_id = db.Column(db.Integer, unique=True, nullable=False)
     datetime_start = db.Column(db.DateTime, nullable=False)
     datetime_end = db.Column(db.DateTime, nullable=False)
 
-class Duty_Type(db.Model):
+class DutyType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     datetime_start = db.Column(db.DateTime, nullable=False)
     datetime_end = db.Column(db.DateTime, nullable=False)
 
-class Duty_Person(db.Model):
+class DutyPerson(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     active = db.Column(db.Boolean, nullable=False)
-    role_id = db.Column(db.DateTime, foreign_key=True, nullable=False)
+    role_id = db.Column(db.Integer, foreign_key=True, nullable=False)
 
-class Duty_Roles_Type(db.Model):
+class DutyRolesType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    role_id = db.Column(db.DateTime, foreign_key=True, nullable=False)
+    role_id = db.Column(db.Integer, foreign_key=True, nullable=False)
 
-class Duty_Roles(db.Model):
+class DutyRole(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)

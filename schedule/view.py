3ab74d949c157ab46c500c36020ae29e918cbf49
evdokimov <o.evdokimov@corp.mail.ru>
2019-06-19 18:59:42 +0300

    create index.html

--- schedule/view.py
+++ schedule/view.py
@@ -5,10 +5,6 @@ from flask import render_template
 
 @app.route("/")
 def index():
-   return render_template('base.html')
-
-@app.route("/calendar")
-def calendar():
-   return render_template('calendar.html')
+   return render_template('index.html')
 
 

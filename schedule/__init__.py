a1727ecd48b9bba9750c232425cdafe531256616
o-evdokimov <elegek@gmail.com>
2019-06-22 14:23:07 +0300

    fix

--- schedule/__init__.py
+++ schedule/__init__.py
@@ -1,20 +1,19 @@
-# __init__.py 
-# init Flask server
+from flask import Flask, render_template
+from schedule.model import db
 
-from flask import Flask
-#from config import Configuration
-#from schedule.model import db
 
 def create_app():
-    #from config import Configuration
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
-    #app.config.from_object(Configuration)
-    #db.init_app(app)
+    db.init_app(app)
+
     @app.route("/")
-    @app.route("/index/")
-    def schedule():
-        return 'schedule2'
+    def index():
+        return render_template('index.html')
+
+    @app.route("/smeny")
+    def smeny():
+        return render_template('smeny.html')
     return app
 
 

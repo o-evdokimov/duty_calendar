0020a2f7e05d1f40135a04823316208944ec558a
evdokimov <o.evdokimov@corp.mail.ru>
2019-07-04 02:42:10 +0300

    add __init__ in /calendar/models.py

--- schedule/calendar/models.py
+++ schedule/calendar/models.py
@@ -6,8 +6,12 @@ db = SQLAlchemy()
  
 class Timeinterval(db.Model):
     id = db.Column(db.Integer, primary_key=True) 
-    time_start = db.Column(db.Time, nullable=False) 
-    time_end = db.Column(db.Time, nullable=False) 
+    time_start = db.Column(db.DateTime, nullable=False) 
+    time_end = db.Column(db.DateTime, nullable=False) 
+
+    def __init__(self, time_start, time_end):
+        self.time_start = time_start
+        self.time_end = time_end
 
 class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,11 +26,16 @@ class Dutytype(db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
     def __repr__(self):
         return ("Dutytype: {}".format(self.name))
+    def __init__(self, *args, **kwargs):
+        super(Dutytype,self).__init__(*args, **kwargs)
 
 class Roleperson(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
     person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+
+    def __init__(self, *args, **kwargs):
+        super(Roleperson,self).__init__(*args, **kwargs)
 
 class Dutyevent(db.Model):
     id = db.Column(db.Integer, primary_key=True)

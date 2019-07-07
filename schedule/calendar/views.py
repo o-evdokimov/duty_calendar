9173eb29e759bd7efd664c7ae4fbcc850692c2b5
evdokimov <o.evdokimov@corp.mail.ru>
2019-07-07 18:32:15 +0300

    вёрстка

--- schedule/calendar/views.py
+++ schedule/calendar/views.py
@@ -4,26 +4,32 @@ from datetime import datetime
 
 
 from schedule.calendar.models import Dutytype, Timeinterval
+from schedule.user.models import Person
 
 blueprint = Blueprint('calendar', __name__, url_prefix='/calendar')
 
+color_btn = ['primary', 'warning', 'success', 'secondary']
+color_row = ['info', 'warning', 'success', 'active']
+
 @blueprint.route('/')
 def index():
-    title = "Рабочий календарь"
-    current_date = datetime.today().strftime('%d-%m-%Y')
-    return render_template('index.html', title = title, date=current_date)
+    title = "Расписание"
+    mydate = datetime.today()
+    persons = Person.query.all()
+    itslen = len(Dutytype.query.all())
+    print('len=',type(itslen))
+    return render_template('index.html', title = title, persons = persons, mydate=mydate, color_btn = color_btn, color_row = color_row, itslen = itslen)
 
 @blueprint.route('/smeny')
 def smeny():
     if current_user.is_authenticated:
         title = "Смены дежурств"
         timeintervals = Timeinterval.query.all()
-        color_btn = ['primary', 'warning', 'success', 'secondary']
-        color_row = ['info', 'warning', 'success', 'active']
+        itslen = len(timeintervals)
         #new_dict = dict(zip(colors, dutytypes))
         #print(new_dict)
-        print(timeintervals)
-        return render_template('smeny.html', timeintervals = timeintervals, color_btn = color_btn, color_row = color_row, title = title)
+        print('len=',type(itslen))
+        return render_template('smeny.html', title = title, timeintervals = timeintervals, color_btn = color_btn, color_row = color_row, itslen = itslen)
     else:
         flash('Log in for access', 'alert-info')
         return redirect(url_for('user.login'))
